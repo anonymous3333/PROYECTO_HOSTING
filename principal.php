fcaaaaeb575932879b44ac4b56ce89de2e02d02b
kevin <kevin.luna200344@gmail.com>
2020-04-25 02:57:04 -0500

    Reestructuración del cuerpo HTML

--- principal.php
+++ principal.php
@@ -1,14 +1,4 @@
-<?php
-
-	session_start();
-
-	if (isset($_SESSION["usuario"])){
-
-		$usuario=$_SESSION["usuario"];
-
-?>
-
-	<!DOCTYPE html>
+<!DOCTYPE html>
 	<html lang="es">
 	<head>
 		<meta charset="UTF-8">
@@ -17,14 +7,37 @@
 		<title>Mi Página Web</title>
 	</head>
 	<body>
+		<!-- Sección de inicio del código php -->
 		<?php
+			session_start();
+			if (isset($_SESSION["usuario"])){
+				$usuario=$_SESSION["usuario"];
+		?>
+		
+		<div align='right'>
+			<table>
+				<tr>
+					<td><?php echo "Usuario:".$usuario ?></td>
 
-			echo "<div align='right'>Usuario: $usuario <a href='cerrar_sesion.php'>Cerrar sesión</a></div><br><br>";
+					<td><a href='cerrar_sesion.php'>Cerrar sesión</a></td>
+					<td></td>
+				</tr>
+			</table>
+		</div>
+		<br><br>
+		<div align='center'>
+		<table class='tabla1'>
+			<tr>
+				<td class='celda1_t1'><h1>BIENVENIDOS A MI PAGINA PRINCIPAL</h1></td>
+			</tr>
+		</table>
+		</div>
+		<br><br>
 
-			echo "<div align='center'><table class='tabla1'><tr><td class='celda1_t1'>BIENVENIDOS A MI PAGINA PRINCIPAL</td></tr></table></div><br><br>";
-
-			echo "<div class='tabla2'>LA FRASE DEL DIA ES: </div>";
-
+		<div class='tabla2'>
+			<h2>La frase del día es:</h2>
+		<!-- Lee e imprime las frases -->
+			<?php
 			$archivo = "frases.txt";
 			$frases = file($archivo);
 			shuffle($frases);
@@ -32,44 +45,47 @@
 			$columnas = explode("&",$frases[0]);
 			echo "<q>".$columnas[0]."</q>"."<br>";
 			echo "<i>".$columnas[1]."</i>";
-
-			echo "<br><br>
+			?>
+			</div>
+			<br><br>
 			<div>
 				<table class='tabla3'>
 					<tr>
-						<td class='celda1_t3'>";
-						$archivo2 = "lugares.txt";
-						$lugares = file($archivo2);
-						shuffle($lugares);
-						//echo $frases[0];
-						$columnas = explode("&",$lugares[0]);
-						echo "<p>Uno de mi lugares favoritos es:<br>";
-						echo "<p>".$columnas[1]."</p>";
-						echo "<img src='img/lugares/".$columnas[0]."'>"."<br>";
-						echo "</td>";
-						echo "<td class='celda2_t3'>";
-						$archivo3 = "mascotas.txt";
-						$mascotas = file($archivo3);
-						shuffle($mascotas);
-						//echo $frases[0];
-						$columnas = explode("&",$mascotas[0]);
-						echo "<p>Unas de mi mascotas favoritas son:<br>";
-						echo "<p>".$columnas[1]."</p>";
-						echo "<img src='img/mascotas/".$columnas[0]."'>"."<br>";
-						echo "</td>
+						<td class='celda1_t3'>
+							<!-- Lee e imprime los lugares -->
+							<?php
+								$archivo2 = "lugares.txt";
+								$lugares = file($archivo2);
+								shuffle($lugares);
+								//echo $frases[0];
+								$columnas = explode("&",$lugares[0]);
+								echo "<p>Uno de mi lugares favoritos es:<br>";
+								echo "<p>".$columnas[1]."</p>";
+								echo "<img src='img/lugares/".$columnas[0]."'>"."<br>";
+							?>
+						</td>
+						<td class='celda2_t3'>
+							<!-- Lee e imprime las mascotas -->
+							<?php
+								$archivo3 = "mascotas.txt";
+								$mascotas = file($archivo3);
+								shuffle($mascotas);
+								//echo $frases[0];
+								$columnas = explode("&",$mascotas[0]);
+								echo "<p>Unas de mi mascotas favoritas son:<br>";
+								echo "<p>".$columnas[1]."</p>";
+								echo "<img src='img/mascotas/".$columnas[0]."'>"."<br>";
+							?>
+						</td>
 					</tr>
 				</table>
-			</div>";
-
-				}
-
-			else{
+			</div>
+		<!-- Fin el if-else -->
+		<?php
+			}else{
 				$prog='acceso.php';
-
 				header("Location: $prog");
-
-}
-
-?>
+			}
+		?>
 </body>
 </html>
