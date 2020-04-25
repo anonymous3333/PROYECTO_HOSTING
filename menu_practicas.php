32f003d7986dd0cc6e372366454e1baf973fec00
kevin <kevin.luna200344@gmail.com>
2020-04-25 18:41:16 -0500

    Menú de prácticas

--- menu_practicas.php
+++ menu_practicas.php
@@ -1,3 +1,9 @@
+<?php
+	session_start();
+	if (isset($_SESSION["usuario"],$_SESSION["visitantes"])):
+		$usuario=$_SESSION["usuario"];
+		$visitante = $_SESSION["visitantes"];
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,6 +13,26 @@
 	<link rel="stylesheet" href="estilos_menu_practicas.css">
 </head>
 <body>
-	
+	<div class="info">
+		<p>Usuario actual:
+			<?=$usuario;?>
+		</p>
+		<p>
+			Número de visitante:
+			<?=$visitante;?>
+		</p>
+	</div>
+	<div class="contenedor_lista">
+		<h1>Menú de prácticas</h1>
+		<a href="#">Sistema Solar</a>
+		<a href="#">Idiomas</a>
+		<a href="#">Figuras</a>
+		<a href="#">IMC</a>
+		<a href="#">Formulario:alta alumnos</a>
+		<a href="principal.php">Volver</a>
+	</div>
 </body>
 </html>
+<?php else:?>
+
+<?php endif;?>
