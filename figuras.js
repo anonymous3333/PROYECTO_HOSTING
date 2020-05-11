650062d7faa90f63110ca603132f82e699432907
kevin <kevin.luna200344@gmail.com>
2020-05-10 19:47:42 -0500

    Funcionalidad Volumenes terminada

---
+++ figuras.js
@@ -0,0 +1,44 @@
+function cargaHTML(nombreArchivo){
+	var frame = document.getElementById("frame");
+	frame.src = "volumenes/"+nombreArchivo+".html"
+}
+function obtenerVolumenCubo(){
+	var arista1 = parseFloat(document.getElementById("arista1").value);
+	var arista2 = parseFloat(document.getElementById("arista2").value);
+	if(isNaN(arista1) || isNaN(arista2))
+		alert("Datos de entrada no válidos.");
+	else{
+		document.getElementById("resultado").value = (arista1*arista2);
+	}
+}
+
+function obtenerVolumenCilindro(){
+	var radio = parseFloat(document.getElementById("radio").value);
+	var altura = parseFloat(document.getElementById("altura").value);
+	if(isNaN(radio) || isNaN(altura))
+		alert("Datos de entrada no válidos.");
+	else{
+		document.getElementById("resultado").value = (Math.PI*radio*radio)*altura;
+	}
+}
+
+function obtenerVolumenCono(){
+	var radio = parseFloat(document.getElementById("radio").value);
+	var altura = parseFloat(document.getElementById("altura").value);
+	if(isNaN(radio) || isNaN(altura))
+		alert("Datos de entrada no válidos.");
+	else{
+		document.getElementById("resultado").value = (1/3)*(Math.PI*radio*radio)*altura;
+	}
+}
+
+function obtenerVolumenPrismaR(){
+	var arista1 = parseFloat(document.getElementById("arista1").value);
+	var arista2 = parseFloat(document.getElementById("arista2").value);
+	var arista3 = parseFloat(document.getElementById("arista3").value);
+	if(isNaN(arista1) || isNaN(arista2) || isNaN(arista3))
+		alert("Datos de entrada no válidos.");
+	else{
+		document.getElementById("resultado").value = (arista1*arista2*arista3);
+	}
+}
---
+++ volumenes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Volúmenes</title>
+	<script src="figuras.js"></script>
+	<link rel="stylesheet" href="volumenes.css">
+</head>
+<body>
+	<div class="contenido-principal">
+		<ol type="1" class="lista-cuerpos" id="lista_planetas">
+			<li><a href="#" onclick="cargaHTML('cubo');">Cubo</a></li>
+			<li><a href="#" onclick="cargaHTML('cilindro');">Cilindro</a></li>
+			<li><a href="#" onclick="cargaHTML('cono');">Cono</a></li>
+			<li><a href="#" onclick="cargaHTML('prisma_rectangular');">Prisma Rectangular</a></li>
+		</ol>
+		<iframe src="" frameborder="0" class="contenedor" id="frame">
+			
+		</iframe>
+	</div>
+</body>
+</html>
