e2746bb436d27796acfd2033967fe88715f36442
kevin <kevin.luna200344@gmail.com>
2020-05-12 01:59:18 -0500

    Funcionalidad Pitagoras Terminada

---
+++ pitagoras.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Teorema de Pitágoras</title>
+	<script src="cargaHTML.js"></script>
+	<link rel="stylesheet" href="practicas-global.css">
+</head>
+<body>
+<div class="contenido-principal">
+		<ol type="1" class="lista-opciones" id="lista_planetas">
+			<li><a href="#" onclick="cargaHTML('pitagoras/calcular-hipotenusa.html');">Calcular hipotenusa</a></li>
+			<li><a href="#" onclick="cargaHTML('pitagoras/calcular-cateto-opuesto.html');">Calcular cateto opuesto</a></li>
+			<li><a href="#" onclick="cargaHTML('pitagoras/calcular-cateto-adyacente.html');">Calcular cateto adyacente</a></li>
+		</ol>
+		<iframe src="pitagoras/calcular-hipotenusa.html" frameborder="0" class="contenedor" id="frame">
+			
+		</iframe>
+	</div>
+</body>
+</html>
