8f310d7df75afba6eebba15192a1323bbf618f18
kevin <kevin.luna200344@gmail.com>
2020-05-20 22:00:48 -0500

    Nuevas pantallas de inicio y cierre de sesión.

--- en/cerrar_sesion.php
+++ en/cerrar_sesion.php
@@ -12,10 +12,52 @@ session_destroy();
 
 ?>
 
-<script type=text/javascript>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>logout</title>
+	<script type="application/javascript">
+		window.onload = function(){
+			setTimeout(() => {
+				location.href = "acceso.php";
+			}, 2000);
+		}
+	</script>
+	<style>
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
 
-alert("Ha cerrado sesión correctamente");
+		body{
+			background-color: #dee2e6;
+		}
 
-</script>
+		h2{
+			text-align:center;
+			font-size: larger;
+		}
 
-<a href="acceso.php">Clic aquí para continuar</a>
+		div{
+			font-family:Arial, Helvetica, sans-serif;
+			color: #212529;
+			margin:auto;
+			margin-top: 10%;
+			height: 100px;
+			width: 30%;
+			padding-top:39px;
+			background-color: #ffc9c9;
+			border: 2px solid #fa5252;
+			border-radius: 10px;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<h2>The session has been closed successfully</h2>
+	</div>
+</body>
+</html>
--- mensaje_acceso.php
+++ mensaje_acceso.php
@@ -1,23 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicio de sesión exitoso</title>
+	<script type="application/javascript">
+		window.onload = function(){
+			setTimeout(() => {
+				location.href = "principal.php";
+			}, 2000);
+		}
+	</script>
+	<style>
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
 
-session_start();
+		body{
+			background-color: #dee2e6;
+		}
 
-if (isset($_SESSION["usuario"]))
+		h2{
+			text-align:center;
+			font-size: larger;
+		}
 
-{ $usuario=$_SESSION["usuario"];
-
-print ("Bienvenido $usuario al contenido restringido de mi p&aacute;gina<br><br>");
-
-print ("<a href='principal.php'>Clic aqui para continuar...</a>");
-
-}
-
-else
-
-{ $prog='acceso.php';
-
-header("Location: $prog");
-
-}
-
-?>
+		div{
+			font-family:Arial, Helvetica, sans-serif;
+			color: #212529;
+			margin:auto;
+			margin-top: 10%;
+			height: 100px;
+			width: 30%;
+			padding-top:39px;
+			background-color: #b2f2bb;
+			border: 2px solid #40c057;
+			border-radius: 10px;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<h2>Has iniciado sesión con éxito</h2>
+	</div>
+</body>
+</html>
